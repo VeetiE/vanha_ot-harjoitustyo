1addae2975039881e95de64d8e1fc57ec646f501
Veeti Enckelman <veeti.oliver@gmail.com>
2022-03-24 20:15:53 +0200

    Luotiin kattavempia testejä kassapaatteeseen ja lisattiin nayttokuva

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -5,6 +5,7 @@ from maksukortti import Maksukortti
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
         self.kassapaate=Kassapaate()
+        self.maksukortti=Maksukortti(1000)
     
     def test_luodun_paatteen_raha_ja_myynntien_maara_on_oikea(self):
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
@@ -41,17 +42,20 @@ class TestKassapaate(unittest.TestCase):
 
     def test_korttiosto_toimii_edullisen_osalta_rahat_riittavat(self):
         
-        self.kassapaate.syo_edullisesti_kortilla(Maksukortti(1000))
+        self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
 
         self.assertEqual(self.kassapaate.edulliset,1)
         self.assertEqual(self.kassapaate.maukkaat,0)
-        self.assertEqual(self.kassapaate.kassassa_rahaa,100000)    
+        self.assertEqual(self.kassapaate.kassassa_rahaa,100000)
+        self.assertEqual(self.maksukortti.saldo,760)    
         
     def test_korttiosto_toimii_maukkaan_osalta_rahat_riittavat(self):
-        self.kassapaate.syo_maukkaasti_kortilla(Maksukortti(1000))
+        self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
         self.assertEqual(self.kassapaate.edulliset,0)
         self.assertEqual(self.kassapaate.maukkaat,1)
-        self.assertEqual(self.kassapaate.kassassa_rahaa,100000)  
+        self.assertEqual(self.kassapaate.kassassa_rahaa,100000)
+        self.assertEqual(self.maksukortti.saldo,600)   
+
 
     def test_korttiosto_toimii_edullisen_osalta_rahat_ei_riita(self):
         self.kassapaate.syo_edullisesti_kortilla(Maksukortti(2))
@@ -73,3 +77,9 @@ class TestKassapaate(unittest.TestCase):
         self.kassapaate.lataa_rahaa_kortille(kortti, 5000)
         self.assertEqual(self.kassapaate.kassassa_rahaa,105000)
         self.assertEqual(kortti.saldo,15000)
+
+    def test_kortin_lataaminen_kasvattaa_kassapaatetta_negatiivinen(self):
+        kortti=Maksukortti(10000)
+        self.kassapaate.lataa_rahaa_kortille(kortti, -5000)
+        self.assertEqual(self.kassapaate.kassassa_rahaa,100000)
+        self.assertEqual(kortti.saldo,10000)
